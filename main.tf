9052836d3aaa55291224b5b2bd65eb9fa2945bfa
David James <djames7776@gmail.com>
2020-03-11 20:48:19 +0000

    addded private subnet

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws"{
 module "vpc" {
     source = "./modules/vpc"
     cidr = "10.1.0.0/16"
+    name = "refresh_vpc"
 }
 
 module "public_subnet" {
@@ -13,4 +14,13 @@ module "public_subnet" {
     vpc = module.vpc.vpc_id
     public_ips = true
     sub_cidr = "10.1.1.0/24"
+    name = "refresh_public_subnet"
+}
+
+module "private_subnet" {
+    source = "./modules/subnet"
+    vpc = module.vpc.vpc_id
+    public_ips = false
+    sub_cidr = "10.1.2.0/24"
+    name = "refresh_private_subnet"
 }
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_name" {
 output "public_subnet_block" {
     value = module.public_subnet.subnet_block
 }
+
+output "private_subnet_name" {
+    value = module.private_subnet.subnet_name
+}
+
+output "private_subnet_block" {
+    value = module.private_subnet.subnet_block
+}
