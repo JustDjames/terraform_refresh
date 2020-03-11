66f8d8986bb42b25fe6ba35713c5d19f3239fe3a
David James <djames7776@gmail.com>
2020-03-11 20:04:53 +0000

    added public subnet

--- main.tf
+++ main.tf
@@ -7,3 +7,10 @@ module "vpc" {
     source = "./modules/vpc"
     cidr = "10.1.0.0/16"
 }
+
+module "public_subnet" {
+    source = "./modules/subnet"
+    vpc = module.vpc.vpc_id
+    public_ips = true
+    sub_cidr = "10.1.1.0/24"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id"{
     value = module.vpc.vpc_id 
 }
+
+output "public_subnet_name" {
+    value = module.public_subnet.subnet_name
+}
+
+output "public_subnet_block" {
+    value = module.public_subnet.subnet_block
+}
