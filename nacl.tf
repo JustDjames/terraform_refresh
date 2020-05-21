b84b01790d016b0b60441c7d8c0336932b6a8279
David James <djames7776@gmail.com>
2020-05-21 19:02:02 +0100

    added ephemeral rules to nacls

--- nacl.tf
+++ nacl.tf
@@ -62,7 +62,7 @@ resource "aws_network_acl_rule" "public_ssh_ingress" {
 
 resource "aws_network_acl_rule" "public_icmp_ingress" {
   network_acl_id = module.public_nacl.nacl_id
-  rule_number          = 600
+  rule_number    = 600
   egress         = false
   protocol       = "icmp"
   rule_action    = "allow"
@@ -71,15 +71,28 @@ resource "aws_network_acl_rule" "public_icmp_ingress" {
   icmp_code      = -1
 }
 
-resource "aws_network_acl_rule" "public_icmp_egress" {
+# allows public instances to reply to requests from the private instances
+resource "aws_network_acl_rule" "private_ephemeral_ingress" {
   network_acl_id = module.public_nacl.nacl_id
-  rule_number          = 600
-  egress         = true
-  protocol       = "icmp"
+  rule_number    = 700
+  egress         = false
+  protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = module.private_subnet.subnet_block
-  icmp_type      = -1
-  icmp_code      = -1
+  from_port      = 32768
+  to_port        = 65535
+}
+
+# allows my pc to reply to requests from the public instances
+resource "aws_network_acl_rule" "public_ephemeral_egress" {
+  network_acl_id = module.public_nacl.nacl_id
+  rule_number = 100
+  egress      = true
+  protocol    = "tcp"
+  rule_action = "allow"
+  cidr_block  = var.local_ip
+  from_port   = 32768
+  to_port     = 65535
 }
 
 # private nacl
@@ -135,13 +148,24 @@ resource "aws_network_acl_rule" "private_icmp_ingress" {
   icmp_code      = -1
 }
 
-resource "aws_network_acl_rule" "private_icmp_egress" {
+resource "aws_network_acl_rule" "private_nat_ephemeral_ingress" {
   network_acl_id = module.private_nacl.nacl_id
-  rule_number    = 400
-  egress         = true
-  protocol       = "icmp"
+  rule_number    = 500
+  egress         = false
+  protocol       = "tcp"
   rule_action    = "allow"
-  cidr_block     = module.public_subnet.subnet_block
-  icmp_type      = -1
-  icmp_code      = -1
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "private_nat_ephemeral_egress" {
+  network_acl_id = module.private_nacl.nacl_id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 1024
+  to_port        = 65535 
 }
